98e0d4f2914b0c0e2e28e5deb64429a63ee72046
jugendhacker <julian.ribbeck@gmx.de>
2016-06-19 10:52:16 +0200

    Final Backend by JH

--- neue_datei.sh
+++ neue_datei.sh
@@ -1,5 +1,5 @@
 sudo cd /var/www/html
 while [ true ]; do
 sudo wget localhost/png.php -O Hello_World.png
-sleep 5
+sleep 2
 done
--- png.php
+++ png.php
@@ -1,10 +1,14 @@
 <?php
+    header("Content-type: image/png");
     //Bild erzeugen
     $img = imagecreate(500, 500);
     //
     $color['lime'] = imagecolorallocate($img, 0x00, 0xFF, 0x00);
     //Schwarze Farbe setzen
     $color['black'] = imagecolorallocate($img, 0x00, 0x00, 0x00);
+    $color['green'] = imagecolorallocate($img, 34, 139, 34);
+    $color['yellow'] = imagecolorallocate($img, 238, 238, 0);
+    $color['red'] = imagecolorallocate($img, 238, 64, 0);
     //Text schreiben
     $mysql_host = "localhost";
     $mysql_db   = "luft";
@@ -15,17 +19,25 @@
     $sql = "SELECT lat, lon, wert FROM `luftmessung`";
     $result = mysql_query($sql, $connection);
     while (($row = mysql_fetch_array($result, MYSQL_NUM))!== FALSE){
-	$a = 51.025904 - $row[0];
+	if($row[2] <= 20){
+            $farbe = 'green';
+        }
+        if($row[2] > 20 && $row[2] <= 35){
+            $farbe = 'yellow';
+        }
+        if($row[2] > 35){
+            $farbe = 'red';
+        }
+        $a = 51.025904 - $row[0];
         $b = 13.723427 - $row[1];
         $latpx = $a * 10239.606799;
         $lonpx = $b * 10239.606799;
         //echo "latpx: $latpx ; lonpx: $lonpx";
 	//echo "latpx: $latpx ; a: $a ; row0: ".$row[0]."<br>";
-        imagestring($img, 400, 250 + $lonpx, 250 + $latpx, $row[2], $color['black']);
+        imagestring($img, 400, 250 + $lonpx, 250 + $latpx, $row[2]." E-6 g/m^3", $color[$farbe]);
     }
     //Hintergrundfarbe entfernen (transparent)
     imagecolortransparent($img, $color['lime']);
     //PNG erzeugen
-    header("Content-type: image/png");
     imagepng($img);
 ?>
