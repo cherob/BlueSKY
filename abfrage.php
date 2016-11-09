7b4af0179053376a366f6f4b35c85ac56c788c23
julian.Ribbeck <julian.ribbeck@gmx.de>
2016-11-09 16:31:53 +0100

    Delet abfage.php

--- abfrage.php
+++ abfrage.php
@@ -1,13 +1,15 @@
 <?php
-    $lat = $_GET["lat"];
-    $lon = $_GET["lon"];
+    $lat = $_GET["lat1"];
+	$lat2 = $_GET["lat2"];
+    $lon = $_GET["lon1"];
+	$lon2 = $_GET["lon2"];
     $mysql_host = "localhost";
     $mysql_db   = "luft";
     $mysql_user = "luft";
     $mysql_pw   = "luft1234";
     $connection = mysql_connect($mysql_host, $mysql_user, $mysql_pw) or die("Verbindung zur Datenbank fehlgeschlagen.");
     mysql_select_db($mysql_db, $connection) or die("Datenbank konnte nicht ausgewaehlt werden.");
-    $sql = "SELECT wert FROM `luftmessung` where `id`=0";
+    $sql = "SELECT wert FROM `luftmessung` where `lat` > ".$lat." && `lat` < ".$lat2." && `lon` > ".$lon." && `lon` < ".$lon2."";
     $result = mysql_query($sql, $connection);
     
     $row = mysql_fetch_array($result, MYSQL_NUM);
